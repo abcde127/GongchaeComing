4630af75138c6c296bd9f2dbbc1307ec7fea8671
uuuuu <uuuuu0207@naver.com>
2026-04-04 18:07:51 +0900

    프로젝트 초기세팅
    init spring boot

---
+++ src/test/java/com/gongchae/gongchae_coming/GongchaeComingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongchae.gongchae_coming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongchaeComingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
