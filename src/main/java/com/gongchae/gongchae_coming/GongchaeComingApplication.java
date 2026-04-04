0a40716b7a042fa2462a900b6b87c510144407b1
uuuuu <uuuuu0207@naver.com>
2026-04-04 18:07:51 +0900

    프로젝트 초기세팅
    init spring boot

---
+++ src/main/java/com/gongchae/gongchae_coming/GongchaeComingApplication.java
@@ -0,0 +1,13 @@
+package com.gongchae.gongchae_coming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongchaeComingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongchaeComingApplication.class, args);
+	}
+
+}
