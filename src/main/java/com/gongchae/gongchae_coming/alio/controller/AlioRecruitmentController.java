93c1b2fc39803072a043ad0acbd18048b3624c03
uuuuu <uuuuu0207@naver.com>
2026-04-26 14:58:07 +0900

    refactor: 목록조회 필터를 별도의 엔드포인트로 분리했던것을 하나로 통합

--- src/main/java/com/gongchae/gongchae_coming/alio/controller/AlioRecruitmentController.java
+++ src/main/java/com/gongchae/gongchae_coming/alio/controller/AlioRecruitmentController.java
@@ -1,7 +1,6 @@
 package com.gongchae.gongchae_coming.alio.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.gongchae.gongchae_coming.alio.dto.AlioFilterOptionResponse;
 import com.gongchae.gongchae_coming.alio.dto.AlioRecruitmentListRequest;
 import com.gongchae.gongchae_coming.alio.service.AlioRecruitmentService;
 import jakarta.validation.Valid;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,40 +20,5 @@ public class AlioRecruitmentController {
 	@GetMapping
 	public JsonNode getRecruitmentList(@Valid @ModelAttribute AlioRecruitmentListRequest request) {
 		return alioRecruitmentService.getRecruitments(request);
-	}
-
-	@GetMapping("/filters/ncs")
-	public List<AlioFilterOptionResponse> getNcsFilterOptions() {
-		return alioRecruitmentService.getNcsFilterOptions();
-	}
-
-	@GetMapping("/filters/work-regions")
-	public List<AlioFilterOptionResponse> getWorkRegionFilterOptions() {
-		return alioRecruitmentService.getWorkRegionFilterOptions();
-	}
-
-	@GetMapping("/filters/hire-types")
-	public List<AlioFilterOptionResponse> getHireTypeFilterOptions() {
-		return alioRecruitmentService.getHireTypeFilterOptions();
-	}
-
-	@GetMapping("/filters/institution-types")
-	public List<AlioFilterOptionResponse> getInstitutionTypeFilterOptions() {
-		return alioRecruitmentService.getInstitutionTypeFilterOptions();
-	}
-
-	@GetMapping("/filters/sort-types")
-	public List<AlioFilterOptionResponse> getSortFilterOptions() {
-		return alioRecruitmentService.getSortFilterOptions();
-	}
-
-	@GetMapping("/filters/sort-directions")
-	public List<AlioFilterOptionResponse> getSortDirectionFilterOptions() {
-		return alioRecruitmentService.getSortDirectionFilterOptions();
-	}
-
-	@GetMapping("/filters/page-sizes")
-	public List<AlioFilterOptionResponse> getPageSizeFilterOptions() {
-		return alioRecruitmentService.getPageSizeFilterOptions();
 	}
 }
